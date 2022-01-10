a2e9b72681370236ea424a8b77ab3845a5abdc42
Brian Jinwright <bjinwright@qwigo.com>
2022-01-09 20:26:38 -0500

    - Added email_template.html file for unit tests

--- pfunk/tests/test_email.py
+++ pfunk/tests/test_email.py
@@ -5,12 +5,12 @@ import os
 from jinja2.exceptions import TemplateNotFound
 
 from pfunk.tests import User, Group
-from pfunk.testcase import CollectionTestCase
+from pfunk.testcase import APITestCase
 from pfunk.contrib.email.ses import SESBackend
 from pfunk.contrib.email.base import EmailBackend
 
 
-class TestEmailBackend(CollectionTestCase):
+class TestEmailBackend(APITestCase):
     collections = [User, Group]
 
     def setUp(self) -> None:
@@ -20,14 +20,12 @@ class TestEmailBackend(CollectionTestCase):
                                 last_name='Lasso', _credentials='abc123', account_status='ACTIVE',
                                 groups=[self.group])
         self.backend = EmailBackend()
-        self.app = self.project.wsgi_app
-        self.c = Client(self.app)
 
     def test_get_template(self):
-        with tempfile.NamedTemporaryFile(suffix='.html') as tmp:
-            template = self.backend.get_template(tmp.name.split("/")[-1])
-            # test jinja render if no exceptions
-            template.render(value="random value")
+
+        template = self.backend.get_template('email/email_template.html')
+        # test jinja render if no exceptions
+        template.render(unittest_value="random value")
         self.assertTrue(True)  # if there are no exceptions, then it is a pass
 
     def test_get_wrong_template(self):
@@ -45,7 +43,7 @@ class TestEmailBackend(CollectionTestCase):
         self.assertIsNotNone(rendered)
 
 
-class TestEmailSES(CollectionTestCase):
+class TestEmailSES(APITestCase):
     collections = [User, Group]
 
     def setUp(self) -> None:
@@ -60,15 +58,15 @@ class TestEmailSES(CollectionTestCase):
 
     @mock.patch('boto3.client')
     def test_send_email(self, mocked):
-        with tempfile.NamedTemporaryFile(suffix='.html') as tmp:
-            res = self.SES.send_email(
-                subject="test",
-                to_emails=["testemail@email.com"],
-                html_template=tmp.name.split("/")[-1],
-                from_email="testFromEmail@email.com",
-                cc_emails=["testCCemail@email.com"],
-                bcc_emails=["testBCCemail@email.com"],
-            )
+
+        res = self.SES.send_email(
+            subject="test",
+            to_emails=["testemail@email.com"],
+            html_template='email/email_template.html',
+            from_email="testFromEmail@email.com",
+            cc_emails=["testCCemail@email.com"],
+            bcc_emails=["testBCCemail@email.com"],
+        )
 
         # if there are no exceptions, then it's a passing test
         self.assertTrue(True)

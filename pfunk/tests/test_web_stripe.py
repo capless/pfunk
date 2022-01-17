0f346b201a9e049bfe5e08199afb315ac7459b31
Juliuz Christian Llanillo <christianllanillo@gmail.com>
2022-01-17 15:04:39 +0800

    Completed CRUD test of StripePackage

--- pfunk/tests/test_web_stripe.py
+++ pfunk/tests/test_web_stripe.py
@@ -16,14 +16,15 @@ class TestWebStripe(APITestCase):
         self.user = User.create(username='test', email='tlasso@example.org', first_name='Ted',
                                 last_name='Lasso', _credentials='abc123', account_status='ACTIVE',
                                 groups=[self.group])
-        self.stripe_pkg = StripePackage.create(
-            stripe_id='100', price='10', description='unit testing...', name='unit test package')
+        self.stripe_pkg = StripePackage.create(group=self.group,
+                                               stripe_id='100', price='10', description='unit testing...', name='unit test package')
         # self.stripe_customer = StripeCustomer.create(user=self.user, customer_id='100', package=self.stripe_pkg)
 
         self.token, self.exp = User.api_login("test", "abc123")
         self.app = self.project.wsgi_app
         self.c = Client(self.app)
-        # self.user.add_permissions(self.group, [PermissionGroup(StripePackage, ['create', 'read', 'write', 'delete'])])
+        self.user.add_permissions(self.group, [PermissionGroup(
+            StripePackage, ['create', 'read', 'write', 'delete'])])
 
     def test_list_package(self):
         res = self.c.get('/stripepackage/list/', headers={
@@ -43,44 +44,55 @@ class TestWebStripe(APITestCase):
             res.json['data']['data']['name'],
             self.stripe_pkg.name)
 
-    # TODO: Fix `forbidden` error in stripe views
     def test_create_package(self):
+        self.assertNotIn("new stripe pkg", [
+            pkg.name for pkg in StripePackage.all()])
         res = self.c.post('/stripepackage/create/',
                           json={
                               'stripe_id': '123',
-                              'name': 'stripe_pkg',
+                              'name': 'new stripe pkg',
                               'price': 10.10,
-                              'description': 'a test package'
+                              'description': 'a test package',
+                              'group': self.group.ref.id()
                           },
                           headers={
-                              "Authorization": self.token,
-                              "Content-Type": "application/json"
+                              "Authorization": self.token
                           })
 
-        print(res.json)
+        self.assertTrue(res.json['success'])
+        self.assertIn("new stripe pkg", [
+                      pkg.name for pkg in StripePackage.all()])
 
-    # TODO: Fix `forbidden` error in stripe views
     def test_update_package(self):
+        self.assertNotIn("updated pkg", [
+            pkg.name for pkg in StripePackage.all()])
+        updated_name = 'updated pkg'
         res = self.c.put(f'/stripepackage/update/{self.stripe_pkg.ref.id()}/',
-                          json={
-                              'stripe_id': '123',
-                              'name': 'stripe_pkg',
-                              'price': 10.10,
-                              'description': 'a test package'
-                          },
-                          headers={
-                              "Authorization": self.token,
-                              "Content-Type": "application/json"
-                          })
+                         json={
+                             'stripe_id': '123',
+                             'name': updated_name,
+                             'price': 10.10,
+                             'description': 'a test package'
+                         },
+                         headers={
+                             "Authorization": self.token,
+                             "Content-Type": "application/json"
+                         })
 
-        print(res.json)
+        self.assertTrue(res.json['success'])
+        self.assertEqual(
+            res.json['data']['data']['name'],
+            updated_name)
 
-    # TODO: Fix `forbidden` error in stripe views
     def test_delete_package(self):
         res = self.c.delete(f'/stripepackage/delete/{self.stripe_pkg.ref.id()}/',
-                          headers={
-                              "Authorization": self.token,
-                              "Content-Type": "application/json"
-                          })
+                            headers={
+                                "Authorization": self.token,
+                                "Content-Type": "application/json"
+                            })
 
-        print(res.json)
+        self.assertTrue(res.json['success'])
+        self.assertNotIn(
+            self.stripe_pkg.ref.id(),
+            [pkg.ref.id() for pkg in StripePackage.all()]
+        )

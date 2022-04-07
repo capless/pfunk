1d767add81be0d267ec4d15c23d50c1489fbe9d7
Juliuz Christian Llanillo <christianllanillo@gmail.com>
2022-04-07 15:29:22 +0800

    Added yaml validator and have it return specific errors

--- pfunk/tests/test_aws.py
+++ pfunk/tests/test_aws.py
@@ -1,20 +1,42 @@
 import unittest
+from unittest import mock
 
 from pfunk.utils.aws import ApiGateway
-from pfunk.tests import User, Group
+from pfunk.tests import User, Group, Person, Sport
 from pfunk.project import Project
 
 
 class ApiGatewayTests(unittest.TestCase):
 
-    def setUp(self) -> None:
-        self.project = Project()
+    @classmethod
+    def setUpCls(cls) -> None:
+        cls.project = Project()
+        cls.aws_client = ApiGateway()
+        cls.project.add_resources([Person, Sport, Group, User])
+        cls.api_yaml = cls.project.generate_swagger()
 
     def test_validate_yaml(self):
-        pass
+        result = self.aws_client.validate_yaml(self.api_yaml)
+        self.assertIsNone(result)  # if there are no errors, then spec is valid
 
+    def test_validate_wrong_yaml(self):
+        result = self.aws_client.validate_yaml('wrong yaml...33::39')
+        self.assertIsNotNone(result)  # if there are returned objs, there is an error
+
+    @mock.patch('boto3.client')
     def test_create_api_from_yaml(self):
-        pass
+        result = self.aws_client.create_api_from_yaml()
+        self.assertTrue(result['success'])
 
+    @mock.patch('boto3.client')
     def test_update_api_from_yaml(self):
-        pass
+        result = self.aws_client.create_api_from_yaml()
+        self.assertTrue(result['success'])
+
+    def test_create_api_from_wrong_yaml(self):
+        result = self.aws_client.create_api_from_yaml('wrong yaml...21320:: asdkas')
+        self.assertEqual(result, 'Bad Request. YAML is not valid.')
+
+    def test_update_api_from_wrong_yaml(self):
+        result = self.aws_client.update_api_from_yaml('wrong yaml...21320:: asdkas')
+        self.assertEqual(result, 'Bad Request. YAML is not valid.')
--- pfunk/utils/aws.py
+++ pfunk/utils/aws.py
@@ -1,11 +1,26 @@
 import boto3
 import swaggyp as sw
+from openapi_spec_validator import validate_v2_spec, openapi_v2_spec_validator
+from openapi_spec_validator.readers import read_from_filename
+from openapi_spec_validator.exceptions import OpenAPIValidationError
+
 
 class ApiGateway(object):
 
     def __init__(self):
         self.client = boto3.client('apigateway')
         pass
+
+    def validate_yaml(self, yaml_file):
+        """ Validate YAML file if it is valid for using OpenAPI Spec v2"""
+        try:
+            spec_dict, spec_url = read_from_filename(yaml_file)
+            validate_v2_spec(spec_dict)
+        except OpenAPIValidationError as err:
+            errors = [{err.message: err.json_path}
+                      for err in openapi_v2_spec_validator.iter_errors(spec_dict)]
+            return errors
+        return None
 
     def create_api_from_yaml(self, yaml_file):
         # response = client.import_rest_api(
@@ -28,9 +43,3 @@ class ApiGateway(object):
         #     body=b'bytes'|file
         # )
         pass
-    
-    def validate_yaml(self, yaml_file):
-        """ Validate YAML file if it is valid for using """
-        pass
-
-    def 

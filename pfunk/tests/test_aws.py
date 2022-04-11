0a959cb57e5f18573fb8a24b44bf7bcac27a399f
Juliuz Christian Llanillo <christianllanillo@gmail.com>
2022-04-11 15:54:06 +0800

    Made yaml validation to properly separate openapi errors and python errors. Refactored swagger.py to return the swagger file directory

--- pfunk/tests/test_aws.py
+++ pfunk/tests/test_aws.py
@@ -1,4 +1,6 @@
+import os
 import unittest
+import tempfile
 from unittest import mock
 
 from pfunk.utils.aws import ApiGateway
@@ -9,34 +11,44 @@ from pfunk.project import Project
 class ApiGatewayTests(unittest.TestCase):
 
     @classmethod
-    def setUpCls(cls) -> None:
+    def setUpClass(cls) -> None:
         cls.project = Project()
         cls.aws_client = ApiGateway()
         cls.project.add_resources([Person, Sport, Group, User])
-        cls.api_yaml = cls.project.generate_swagger()
+
+        swagger = cls.project.generate_swagger()
+        cls.swagger_dir = swagger['dir']
+        cls.swagger_file = swagger['swagger_file']
+        print(cls.swagger_dir)
 
     def test_validate_yaml(self):
-        result = self.aws_client.validate_yaml(self.api_yaml)
+        result = self.aws_client.validate_yaml(self.swagger_dir)
         self.assertIsNone(result)  # if there are no errors, then spec is valid
 
     def test_validate_wrong_yaml(self):
         result = self.aws_client.validate_yaml('wrong yaml...33::39')
-        self.assertIsNotNone(result)  # if there are returned objs, there is an error
+        # if there are returned objs, there is an error
+        self.assertIsNotNone(result)
 
     @mock.patch('boto3.client')
-    def test_create_api_from_yaml(self):
-        result = self.aws_client.create_api_from_yaml()
+    def test_create_api_from_yaml(self, mocked):
+        result = self.aws_client.create_api_from_yaml(
+            yaml_file=self.swagger_dir)
         self.assertTrue(result['success'])
 
     @mock.patch('boto3.client')
-    def test_update_api_from_yaml(self):
-        result = self.aws_client.create_api_from_yaml()
-        self.assertTrue(result['success'])
+    def test_create_api_from_wrong_yaml(self, mocked):
+        with tempfile.NamedTemporaryFile(suffix='.yaml', mode='w') as tmp:
+            tmp.seek(0)
+            tmp.write('test wrong yaml')
+            result = self.aws_client.create_api_from_yaml(tmp.name)
+        self.assertEqual(result['error'], 'Bad Request. YAML is not valid.')
 
-    def test_create_api_from_wrong_yaml(self):
-        result = self.aws_client.create_api_from_yaml('wrong yaml...21320:: asdkas')
-        self.assertEqual(result, 'Bad Request. YAML is not valid.')
+    # @mock.patch('boto3.client')
+    # def test_update_api_from_yaml(self):
+    #     result = self.aws_client.update_api_from_yaml(yaml_file=self.api_yaml)
+    #     self.assertTrue(result['success'])
 
-    def test_update_api_from_wrong_yaml(self):
-        result = self.aws_client.update_api_from_yaml('wrong yaml...21320:: asdkas')
-        self.assertEqual(result, 'Bad Request. YAML is not valid.')
+    # def test_update_api_from_wrong_yaml(self):
+    #     result = self.aws_client.update_api_from_yaml('wrong yaml...21320:: asdkas')
+    #     self.assertEqual(result, 'Bad Request. YAML is not valid.')
--- pfunk/utils/aws.py
+++ pfunk/utils/aws.py
@@ -1,36 +1,86 @@
 import boto3
 import swaggyp as sw
+# from botocore.exceptions import BadReq
+from envs import env
 from openapi_spec_validator import validate_v2_spec, openapi_v2_spec_validator
 from openapi_spec_validator.readers import read_from_filename
 from openapi_spec_validator.exceptions import OpenAPIValidationError
 
 
 class ApiGateway(object):
+    region_name = env('SES_REGION_NAME', 'us-east-1')
 
     def __init__(self):
-        self.client = boto3.client('apigateway')
-        pass
+        self.client = boto3.client('apigateway', region_name=self.region_name)
 
     def validate_yaml(self, yaml_file):
         """ Validate YAML file if it is valid for using OpenAPI Spec v2"""
         try:
             spec_dict, spec_url = read_from_filename(yaml_file)
             validate_v2_spec(spec_dict)
+        except (OSError, AttributeError) as err:
+            return {'errors': str(err)}
         except OpenAPIValidationError as err:
+            return self._iterate_validator_errors(spec_dict)
+        return None
+
+    def _iterate_validator_errors(self, spec_dict):
+        """ Iterates through list of errors that the `openapi_spec_validator` returned 
+        
+            This method was implemented due to `openapi_spec_validator` design
+            that if an error happened while iterating through the YAML file
+            it returns a Python error.
+
+            Args:
+                spec_dict (dict, required):
+                    `spec_dict` generated from `openapi_spec_validator.readers.read_from_filename`
+            Returns:
+                list of errors
+        """
+        try:
             errors = [{err.message: err.json_path}
                       for err in openapi_v2_spec_validator.iter_errors(spec_dict)]
             return errors
-        return None
+        except (OSError, AttributeError) as err:
+            return str(err)
 
-    def create_api_from_yaml(self, yaml_file):
-        # response = client.import_rest_api(
-        #     failOnWarnings=True|False,
-        #     parameters={
-        #         'string': 'string'
-        #     },
-        #     body=b'bytes'|file
-        # )
-        pass
+    def create_api_from_yaml(self, yaml_file, fail_on_warnings=True):
+        """ Creates an API for AWS API Gateway from a YAML swagger file 
+        
+        Args:
+            yaml_file (yaml file, required):
+                The OpenAPI swagger file to create API from
+            fail_on_warnings (bool, optional):
+                Specifies if the method will error on warnings. Default: `True` 
+        """
+        _yaml_valid = self.validate_yaml(yaml_file)
+        if _yaml_valid:
+            return {
+                "error": 'Bad Request. YAML is not valid.',
+                "yaml_err": _yaml_valid
+            }
+
+        try:
+            if not type(yaml_file) == 'string':
+                with open(yaml_file, 'r') as file:
+                    response = self.client.import_rest_api(
+                        failOnWarnings=fail_on_warnings,
+                        body=file
+                    )
+            else:
+                response = self.client.import_rest_api(
+                    failOnWarnings=fail_on_warnings,
+                    body=yaml_file
+                )
+
+            if response:
+                return {
+                    'success': True,
+                    response: response
+                }
+        # TODO: Specify boto exceptions
+        except Exception as err:
+            return err
 
     def update_api_from_yaml(self, yaml_file):
         # response = client.put_rest_api(

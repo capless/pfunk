ce9cdf9e8ec52b2b2ca647260635073d65ef365a
Juliuz Christian Llanillo <christianllanillo@gmail.com>
2022-04-12 16:15:50 +0800

    Finished create/update api from yaml. Added writing to config file if API is created. Added reading from config file if API is to be updated

--- pfunk/tests/test_aws.py
+++ pfunk/tests/test_aws.py
@@ -11,7 +11,8 @@ from pfunk.project import Project
 class ApiGatewayTests(unittest.TestCase):
 
     @classmethod
-    def setUpClass(cls) -> None:
+    @mock.patch('boto3.client')
+    def setUpClass(cls, mocked) -> None:
         cls.project = Project()
         cls.aws_client = ApiGateway()
         cls.project.add_resources([Person, Sport, Group, User])
@@ -43,11 +44,17 @@ class ApiGatewayTests(unittest.TestCase):
             result = self.aws_client.create_api_from_yaml(tmp.name)
         self.assertEqual(result['error'], 'Bad Request. YAML is not valid.')
 
-    # @mock.patch('boto3.client')
-    # def test_update_api_from_yaml(self):
-    #     result = self.aws_client.update_api_from_yaml(yaml_file=self.api_yaml)
-    #     self.assertTrue(result['success'])
+    @mock.patch('boto3.client')
+    def test_update_api_from_yaml(self, mocked):
+        result = self.aws_client.update_api_from_yaml(
+            yaml_file=self.swagger_dir, mode='merge')
+        print(result)
+        self.assertTrue(result['success'])
 
-    # def test_update_api_from_wrong_yaml(self):
-    #     result = self.aws_client.update_api_from_yaml('wrong yaml...21320:: asdkas')
-    #     self.assertEqual(result, 'Bad Request. YAML is not valid.')
+    @mock.patch('boto3.client')
+    def test_update_api_from_wrong_yaml(self, mocked):
+        with tempfile.NamedTemporaryFile(suffix='.yaml', mode='w') as tmp:
+            tmp.seek(0)
+            tmp.write('test wrong yaml')
+            result = self.aws_client.update_api_from_yaml(tmp.name, mode='merge')
+        self.assertEqual(result['error'], 'Bad Request. YAML is not valid.')
